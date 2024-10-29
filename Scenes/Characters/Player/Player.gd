7613f0525bdc1933ba92d538298260acfaec6341
HernanMGC <hernan.gonzalez.calderon@gmail.com>
2024-10-29 19:55:33 +0100

    Movement changed to use navigation region 2D

--- Scenes/Characters/Player/Player.gd
+++ Scenes/Characters/Player/Player.gd
@@ -1,14 +1,17 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 400
 @export var reachThreshold = 10
 
-var goToPosition : Vector2 = Vector2.ZERO
-var currentDirection : Vector2 = Vector2.ZERO
+#var currentDirection : Vector2 = Vector2.ZERO
 var isMoving : bool = false
+var navigation2DAgent : NavigationAgent2D = null
+var targetPosition : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	navigation2DAgent = $NavigationAgent2D
+	targetPosition = global_position
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
@@ -20,20 +23,31 @@ func _input(event: InputEvent) -> void:
 	if event.is_action("GoToPosition"):
 		print("Is action GoToPosition ")
 		set_is_moving(true)
-		goToPosition = event.position
-		currentDirection = (goToPosition - position).normalized()
+		var map = get_world_2d().navigation_map
+		var goToPosition = NavigationServer2D.map_get_closest_point(map, event.position)
+		navigation2DAgent.target_position = goToPosition
 
 func update_position(delta : float) -> void:
-	var direction : Vector2 = (goToPosition - position).normalized()
-	
-	var velocity : Vector2 = direction * speed
-	var newPosition : Vector2 = position + velocity * delta
-	var distance : float = goToPosition.distance_squared_to(newPosition)
-	if (distance <= reachThreshold):
+	#var direction : Vector2 = (navigation2DAgent.get_next_path_position() - position).normalized()
+	#
+	#var velocity : Vector2 = direction * speed
+	#var newPosition : Vector2 = position + velocity * delta
+#
+	#if (navigation2DAgent.navigation_finished):
+		#set_is_moving(false)
+	#
+	#position = newPosition
+	pass
+
+func _physics_process(delta: float) -> void:
+	if (navigation2DAgent.is_navigation_finished()):
 		set_is_moving(false)
-		newPosition = goToPosition
+		return
 	
-	position = newPosition
+	var diff : Vector2 = navigation2DAgent.get_next_path_position() - global_position
+	var dir : Vector2 = diff.normalized()
+	velocity = dir * speed
+	move_and_slide()
 	pass
 
 func set_is_moving(newIsMoving : bool) -> void:
